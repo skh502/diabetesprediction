3b37b1d5449f9cf93583f29e5bd26f3270da5f51
skh502 <sagar.khanal502@gmail.com>
2025-01-30 18:03:32 +0545

    changes

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import joblib
+# import joblib
 from prediction import predict_svc, predict_knn  
 
 # Define the feature names used in training
